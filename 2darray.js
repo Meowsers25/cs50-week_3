11dc402fcf6e010b2d45bd903c4cba835fcbcc56
Christian D. Lamothe <c.lamothe25@gmail.com>
2017-11-12 00:58:22 +0000

    class practice

---
+++ 2darray.js
@@ -0,0 +1,11 @@
+var array2d = [[4, 3], [5, 1, 8]];
+
+//console.log(array2d[1][0]);
+
+for (var i = 0; i < array2d.length; i++)
+{
+    for (var j = 0; j < array2d[i].length; j++)
+    {
+       console.log(array2d[i][j]);
+    }
+}
---
+++ classpractice.c
@@ -0,0 +1,39 @@
+
+
+
+
+
+//constants
+#define DIM_MIN 3
+#define DIM_MAX 9
+
+//board
+int board[DIM_MAX][DIM_MAX];
+
+//dimensions
+int d;
+
+void init(void);
+void
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+
+printf("%i", board[4][5]);
+
+init();
